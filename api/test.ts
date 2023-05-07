351f3e28cdb7bac80c259cf129eaed1fc5cfed7d
alpharmi <gianni.s177@outlook.com>
2023-05-07 17:53:00 +1000

    Update test.ts

--- api/test.ts
+++ api/test.ts
@@ -2,7 +2,8 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 export default function handler(request: VercelRequest, response: VercelResponse) {
-    fetch("https://api-os-takumi.mihoyo.com/common/gacha_record/api/getGachaLog?").then(response => response.json()).then(data => {
-        response.end(data)
+    fetch("https://api-os-takumi.mihoyo.com/common/gacha_record/api/getGachaLog?").then(data => {
+        console.log(data)
+        response.end("test")
     })
 }
