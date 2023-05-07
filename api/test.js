fc7ab37858bd0627628ffcbad026dce7030d9717
alpharmi <gianni.s177@outlook.com>
2023-05-07 18:04:38 +1000

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-    fetch("https://api-os-takumi.mihoyo.com/common/gacha_record/api/getGachaLog?" + query).then(response => response.json()).then(data => {
+    fetch("https://api-os-takumi.mihoyo.com/common/gacha_record/api/getGachaLog?").then(response => response.json()).then(data => {
         res.end(data)
     })
 }
