81320cc3d5c14573ccc71207a7aacd5fa34ff3c3
alpharmi <gianni.s177@outlook.com>
2023-05-17 16:11:21 +1000

    header

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="fixed flex justify-between items-center top-0 w-full h-14 px-5 backdrop-blur-lg backdrop-brightness-75 border-b-[rgba(255,_255,_255,_0.08)] border-b z-50">
         <div class="font-bold font-sans text-xl flex flex-row gap-1">
-            <p class="text-description">astral express</p>
+            <p class="text-description link-hidden" @click="redirect('/')">astral express</p>
             <p v-for="route in currentRoute()" :id="route" class="fade-in"><span class="text-description">/ </span>{{ route }}</p>
         </div>
     </div>
@@ -22,4 +22,10 @@
     import * as vueRouter from "vue-router"
 
     const router = vueRouter.useRouter()
+
+    function redirect(route) {
+        router.push({
+            path: route,
+        })
+    }
 </script>
